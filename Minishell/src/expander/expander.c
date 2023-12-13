6362aee2906cde3b907f3350406bbe3efc789b4d
syan <113340927+Andre1YAN@users.noreply.github.com>
2023-12-13 14:14:45 +0100

    Add files via upload

--- Minishell/src/expander/expander.c
+++ Minishell/src/expander/expander.c
@@ -6,11 +6,47 @@
 /*   By: syan <syan@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/12 10:45:25 by syan              #+#    #+#             */
-/*   Updated: 2023/12/12 15:49:10 by syan             ###   ########.fr       */
+/*   Updated: 2023/12/13 14:00:37 by syan             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/minishell.h"
+
+void	expander_get_offsets(char *token, int start, int end, char **final_str)
+{
+	char	*aux;
+
+	aux = ft_substr(token, start, (end - start));
+	ft_strupdate(final_str, ft_strjoin(*final_str, aux));
+	free(aux);
+}
+
+char	*expand_variables(char *token)
+{
+	int		i;
+	int		start;
+	char	*envar;
+	char	*final_str;
+
+	i = -1;
+	start = 0;
+	final_str = ft_strdup("");
+	while (token[++i])
+	{
+		if (token[i] == DOLLAR_SIGN)
+		{
+			if (check_last_expansion_occurrence(token) != INDEX_ZERO)
+				expander_get_offsets(token, start, i, &final_str);
+			envar = is_envar_expansible(token, &i, &final_str);
+			if (envar != NULL)
+				return (NULL);
+			start = i + 1;
+		}
+		if (i == check_last_expansion_occurrence(token))
+			expander_get_offsets(token, i + 1, ft_strlen(token), &final_str);
+	}
+	return (final_str);
+}
 
 t_list	*empty_case(t_list *node, t_list *prev)
 {
@@ -29,7 +65,7 @@ char	*minishell_expansion(char *token)
 {
 	char	*str;
 	
-	str = case_that_are_not_expansible(token);
+	str = cases_that_are_not_expansible(token);
 	if (str != NULL)
 		return (str);
 	str = expand_variables(token);
